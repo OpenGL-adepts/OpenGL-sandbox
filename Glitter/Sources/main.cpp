461b69ad8b062d837dc05969c1163234f593d280
pekfos <pekfos@tlen.pl>
2018-05-21 17:38:32 +0200

    Initial commit. Added Glitter + ImGui project with CMake

---
+++ Glitter/Sources/main.cpp
@@ -0,0 +1,61 @@
+#include "glitter.hpp"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw_gl3.h>
+#include <cstdio>
+#include <cstdlib>
+
+
+int main()
+{
+	// Load GLFW and Create a Window
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
+
+	// Check for Valid Context
+	if (mWindow == nullptr) {
+		fprintf(stderr, "Failed to Create OpenGL Context");
+		return EXIT_FAILURE;
+	}
+
+	// Create Context and Load OpenGL Functions
+	glfwMakeContextCurrent(mWindow);
+	gladLoadGL();
+	fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+
+	// Setup ImGui
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui_ImplGlfwGL3_Init(mWindow, true);
+	glfwSwapInterval(1);
+
+	// Rendering Loop
+	while (!glfwWindowShouldClose(mWindow))
+	{
+		if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(mWindow, true);
+
+		// Background Fill Color
+		glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		ImGui_ImplGlfwGL3_NewFrame();
+		ImGui::Text("Hello, word!");
+		ImGui::Render();
+		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
+		// Flip Buffers and Draw
+		glfwSwapBuffers(mWindow);
+		glfwPollEvents();
+	}
+	
+	ImGui_ImplGlfwGL3_Shutdown();
+	glfwTerminate();
+}
