9bf8575da39863e222ead187342fa71490971ae6
pekfos <pekfos@tlen.pl>
2018-05-22 01:12:04 +0200

    Fixed shader not being activated

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -60,6 +60,7 @@ int main()
 		glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		shader.activate();
 		model.draw(shader.get());
 
 		ImGui_ImplGlfwGL3_NewFrame();
